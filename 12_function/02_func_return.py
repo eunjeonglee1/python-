e69f4389961b45ec7abae13fbc4c44686bf0c12a
eunjeong <ej602kr@gmail.com>
2024-02-24 01:06:07 +0900

    commit3

---
+++ 12_function/02_func_return.py
@@ -0,0 +1,37 @@
+# 함수 반환문 return
+
+def get_area():
+    w=int(input('가로길이 :'))
+    h=int(input('세로길이 :'))
+    result=w*h
+    print(f'사각형 가로={w}, 세로={h}, 면적은 {result}')
+    return result
+
+print(get_area())
+
+def multi_return():
+    return 1,2,3
+
+# value=multi_return()
+# w,h,area=value
+# print(value,type(value))
+# print(w,h,area)
+
+def gat_names():
+    names=[]
+    for i in range(3):
+        name=input('이름입력:')
+        names.append(name)
+    return names
+
+names=gat_names()
+print(names)
+
+def get_info():
+    name=input('이름입력:')
+    age=input('나이:')
+    info={'name':name,'age':age}
+    return info
+
+info=get_info()
+print(info,type(info))
